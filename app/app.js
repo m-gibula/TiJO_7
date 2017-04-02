cbbc651ad760a59e2a9da5d0518510a1263733d3
aniaw <aniaw@users.noreply.github.com>
2017-04-02 08:57:09 +0200

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,62 @@
+function isPalindrome(str)
+{
+    var strTemp = str.toLowerCase(),
+            strLength = strTemp.length;
+    if (str === '') {
+        return false;
+    }
+    var halfLength = (strLength % 2 === 0) ? (strLength / 2) : ((strLength - 1) / 2);
+    for (var i = 0; i < halfLength; i++) {
+        if (strTemp[i] !== strTemp.slice(-1 - i)[0]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function vowelCount(str)
+{
+    var vowelList = 'aeiouyAEIOUY';
+    var vovCount = 0;
+    for (var i = 0, strLength = str.length; i < strLength; i++) {
+        if (vowelList.indexOf(str[i]) !== -1) {
+            vovCount++;
+        }
+    }
+    return vovCount;
+}
+
+function generateMessage(text)
+{
+    if (!!text && text.length > 0) {
+        var palindrome = this.isPalindrome(text);
+        var vowel = this.vowelCount(text);
+        var message = '';
+
+        if (palindrome) {
+            if (vowel > 0) {
+                message = text + ' is palindrome and has ' + vowel + ' vovels';
+            }
+            else {
+                message = text + ' is palindrome and has no vovels';
+            }
+        }
+        else {
+            if (vowel > 0) {
+                message = text + ' is not palindrome and has ' + vowel + ' vovels';
+            }
+            else {
+                message = text + ' is not palindrome and has no vovels';
+            }
+        }
+        return {vowel: vowel, palindrome: palindrome, message: message};
+    } else {
+        throw new Error('Argument is undefined, not a string or empty string!');
+    }
+}
+
+module.exports = {
+    generateMessage: generateMessage,
+    isPalindrome: isPalindrome,
+    vowelCount: vowelCount
+};
---
+++ tests/app.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var app = require('../app/app');
+
+describe('generateMessage without sinon.spy and sinon.stub', function ()
+{
+
+});
+
+describe('generateMessage with sinon.spy adn sinon.stub', function ()
+{
+    describe('spy', function ()
+    {
+        describe('callCount', function ()
+        {
+        });
+        describe('calledWith', function ()
+        {
+        });
+    });
+
+    describe('stub', function ()
+    {
+        describe('returns', function ()
+        {
+
+        });
+        describe('withArgs', function ()
+        {
+
+        });
+        describe('callsFake', function ()
+        {
+
+        });
+    });
+});
