41f0a39ceef65cdb56b3651586571682c18b2609
Mateusz Gibuła <mgibula94@gmail.com>
2017-06-06 18:30:11 +0200

    generateMessage() tests

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,37 +1,28 @@
 'use strict';
-
+var expect = require('chai').expect;
 var app = require('../app/app');
+var chai = require('chai');
+var sinon  = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
-describe('generateMessage without sinon.spy and sinon.stub', function ()
+
+describe('generateMessage() tests', function ()
 {
+    it('should generate message when word is palindrome',function(){
+        expect(app.generateMessage('ala')).to.eql({vowel:2, palindrome: true, message: 'ala is palindrome and has 2 vovels'})
+        expect(app.generateMessage('lll')).to.eql({vowel:0, palindrome: true, message: 'lll is palindrome and has no vovels'})
+    })
 
-});
+    it('should generate message when word is not palindrome',function(){
+        expect(app.generateMessage('kot')).to.eql({vowel:1, palindrome: false, message: 'kot is not palindrome and has 1 vovels'})
+        expect(app.generateMessage('lsg')).to.eql({vowel:0, palindrome: false, message: 'lsg is not palindrome and has no vovels'})
+    })
 
-describe('generateMessage with sinon.spy adn sinon.stub', function ()
-{
-    describe('spy', function ()
-    {
-        describe('callCount', function ()
-        {
-        });
-        describe('calledWith', function ()
-        {
-        });
-    });
+    it('should throw expception when arguments are wrong', function(){
+        expect(function(){app.generateMessage('')}).to.throw('Empty String!');
+        expect(function () {app.generateMessage('123').to.throw('Not valid!');
+        })
+    })
 
-    describe('stub', function ()
-    {
-        describe('returns', function ()
-        {
-
-        });
-        describe('withArgs', function ()
-        {
-
-        });
-        describe('callsFake', function ()
-        {
-
-        });
-    });
 });
